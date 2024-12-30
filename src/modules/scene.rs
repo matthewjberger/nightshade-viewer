fc7775f4b0985d07c53eb8aaeaabf461c472429e
Matthew J. Berger <matthewberger@nevada.unr.edu>
2024-12-30 00:41:00 -0700

    add camera component

--- src/modules/scene.rs
+++ src/modules/scene.rs
@@ -1,7 +1,8 @@
 crate::ecs! {
     Context {
-        local_transform: LocalTransform => LOCAL_TRANSFORM,
+        camera: Camera => CAMERA,
         global_transform: GlobalTransform => GLOBAL_TRANSFORM,
+        local_transform: LocalTransform => LOCAL_TRANSFORM,
         name: GlobalTransform => NAME,
         parent: Parent => PARENT,
         visible: Visible => VISIBLE,
@@ -15,34 +16,151 @@ crate::ecs! {
     }
 }
 
-#[derive(Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
-pub struct LocalTransform {
-    pub translation: nalgebra_glm::Vec3,
-    pub rotation: nalgebra_glm::Quat,
-    pub scale: nalgebra_glm::Vec3,
-}
+pub use components::*;
+pub mod components {
+    #[derive(Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+    pub struct LocalTransform {
+        pub translation: nalgebra_glm::Vec3,
+        pub rotation: nalgebra_glm::Quat,
+        pub scale: nalgebra_glm::Vec3,
+    }
 
-impl Default for LocalTransform {
-    fn default() -> Self {
-        Self {
-            translation: nalgebra_glm::Vec3::new(0.0, 0.0, 0.0),
-            rotation: nalgebra_glm::Quat::identity(),
-            scale: nalgebra_glm::Vec3::new(1.0, 1.0, 1.0),
+    impl Default for LocalTransform {
+        fn default() -> Self {
+            Self {
+                translation: nalgebra_glm::Vec3::new(0.0, 0.0, 0.0),
+                rotation: nalgebra_glm::Quat::identity(),
+                scale: nalgebra_glm::Vec3::new(1.0, 1.0, 1.0),
+            }
+        }
+    }
+
+    #[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+    pub struct GlobalTransform(pub nalgebra_glm::Mat4);
+
+    #[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+    pub struct Name(pub String);
+
+    #[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+    pub struct Parent(pub crate::modules::scene::EntityId);
+
+    #[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
+    pub struct Visible;
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+    pub struct Camera {
+        pub projection: Projection,
+        pub viewport: Viewport,
+        pub tile_id: Option<egui_tiles::TileId>,
+    }
+
+    impl Camera {
+        pub fn projection_matrix(&self, aspect_ratio: f32) -> nalgebra_glm::Mat4 {
+            match &self.projection {
+                Projection::Perspective(camera) => camera.matrix(aspect_ratio),
+                Projection::Orthographic(camera) => camera.matrix(),
+            }
+        }
+    }
+
+    impl Default for Camera {
+        fn default() -> Self {
+            Self {
+                projection: Projection::default(),
+                viewport: Viewport::default(),
+                tile_id: None,
+            }
+        }
+    }
+
+    #[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize)]
+    pub struct Viewport {
+        pub x: u32,
+        pub y: u32,
+        pub width: u32,
+        pub height: u32,
+    }
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+    pub enum Projection {
+        Perspective(PerspectiveCamera),
+        Orthographic(OrthographicCamera),
+    }
+
+    impl Default for Projection {
+        fn default() -> Self {
+            Self::Perspective(PerspectiveCamera::default())
+        }
+    }
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+    pub struct PerspectiveCamera {
+        pub aspect_ratio: Option<f32>,
+        pub y_fov_rad: f32,
+        pub z_far: Option<f32>,
+        pub z_near: f32,
+    }
+
+    impl Default for PerspectiveCamera {
+        fn default() -> Self {
+            Self {
+                aspect_ratio: None,
+                y_fov_rad: 90_f32.to_radians(),
+                z_far: None,
+                z_near: 0.01,
+            }
+        }
+    }
+
+    impl PerspectiveCamera {
+        pub fn matrix(&self, viewport_aspect_ratio: f32) -> nalgebra_glm::Mat4 {
+            let aspect_ratio = if let Some(aspect_ratio) = self.aspect_ratio {
+                aspect_ratio
+            } else {
+                viewport_aspect_ratio
+            };
+
+            if let Some(z_far) = self.z_far {
+                nalgebra_glm::perspective_zo(aspect_ratio, self.y_fov_rad, self.z_near, z_far)
+            } else {
+                nalgebra_glm::infinite_perspective_rh_zo(aspect_ratio, self.y_fov_rad, self.z_near)
+            }
+        }
+    }
+
+    #[derive(Default, Debug, serde::Serialize, serde::Deserialize, Clone)]
+    pub struct OrthographicCamera {
+        pub x_mag: f32,
+        pub y_mag: f32,
+        pub z_far: f32,
+        pub z_near: f32,
+    }
+
+    impl OrthographicCamera {
+        pub fn matrix(&self) -> nalgebra_glm::Mat4 {
+            let z_sum = self.z_near + self.z_far;
+            let z_diff = self.z_near - self.z_far;
+            nalgebra_glm::Mat4::new(
+                1.0 / self.x_mag,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0 / self.y_mag,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                2.0 / z_diff,
+                0.0,
+                0.0,
+                0.0,
+                z_sum / z_diff,
+                1.0,
+            )
         }
     }
 }
-
-#[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
-pub struct GlobalTransform(pub nalgebra_glm::Mat4);
-
-#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
-pub struct Name(pub String);
-
-#[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
-pub struct Parent(pub crate::modules::scene::EntityId);
-
-#[derive(Default, Debug, Copy, Clone, serde::Serialize, serde::Deserialize, PartialEq)]
-pub struct Visible;
 
 pub mod queries {
     /// Queries for root nodes by looking for entities that do not have a Parent component
